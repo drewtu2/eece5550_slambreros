f969b014a1bfa0976d27cb3511b026c593f5ab04
Andrew Tu <drewtu2@yahoo.com>
2019-04-17 16:16:10 -0400

    stuff...

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 
+# Install Multirobot Map Merge
 sudo apt install ros-${ROS_DISTRO}-multirobot-map-merge
+
+# Install Multirobot Explore Lite (Frontier Exploration)
+sudo apt install ros-${ROS_DISTRO}-multirobot-map-merge ros-${ROS_DISTRO}-explore-lite
